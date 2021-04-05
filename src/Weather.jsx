2b3fa7f227fb2bc5e61f13614f2a0e42df253e4c
jayceeth <75772603+jayceeth@users.noreply.github.com>
2021-04-04 20:16:52 -0700

    Edited the API URL

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -19,7 +19,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
       date: new Date(response.data.dt * 1000),
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
 
