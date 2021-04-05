15513f1e32dd12a5ca0d47957519153f2ca92acd
jayceeth <75772603+jayceeth@users.noreply.github.com>
2021-04-04 20:12:26 -0700

    Added 2 rows of forecast

---
+++ src/ForecastOne.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import "./ForecastOne.css";
+import WeatherForecastDaily from "./WeatherForecastDaily";
+import axios from "axios";
+
+export default function ForecastOne(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    return (
+      <div className="ForecastOne">
+        <div className="row">
+          {forecast.map(function (dailyForecastOne, index) {
+            if (index < 3) {
+              return (
+                <div className="col-2 weather-forecast-one" key={index}>
+                  <WeatherForecastDaily data={dailyForecastOne} />
+                </div>
+              );
+            }
+            return <></>;
+          })}
+          {forecast.map(function (dailyForecastTwo, index) {
+            if ((index > 2) & (index < 6)) {
+              return (
+                <div className="col-2 weather-forecast-two" key={index}>
+                  <WeatherForecastDaily data={dailyForecastTwo} />
+                </div>
+              );
+            }
+            return <></>;
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    let apiKey = "f0229aa4803b78f326fa1951e4c8d9a5";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+    axios.get(apiURL).then(handleResponse);
+    return null;
+  }
+}
